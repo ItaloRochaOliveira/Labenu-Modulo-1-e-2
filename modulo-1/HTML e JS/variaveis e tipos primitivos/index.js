02760416dcab2c5b97abb924c96a589e6a1b1eff
Italo Rocha <italo.rocha.de.oliveira@gmail.com>
2022-11-21 16:29:47 -0300

    First push

---
+++ modulo-1/HTML e JS/variaveis e tipos primitivos/index.js
@@ -0,0 +1,19 @@
+console.log("Óla mundo!")
+
+const nome = "Italo"
+console.log(typeof nome)
+
+let idade = 21
+console.log(typeof idade)
+
+console.log("Olá, meu nome é", nome, "e tenho", idade, "anos")
+
+const objeto = "bicicleta"
+const pineus = 2
+const temGuidom = true
+
+console.log(typeof objeto)
+console.log(typeof pineus)
+console.log(typeof temGuidom)
+
+console.log("A minha", objeto, "tem", pineus, "pineus e o valor para se tem guidom é", temGuidom)
