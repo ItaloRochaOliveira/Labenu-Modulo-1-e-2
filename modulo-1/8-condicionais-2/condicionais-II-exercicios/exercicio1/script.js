25fd80a77167f6a13de3d6ed6cbefa66a8c49686
Italo Rocha <italo.rocha.de.oliveira@gmail.com>
2022-12-01 13:05:53 -0300

    subindo atividade de objeto para feedback

---
+++ modulo-1/8-condicionais-2/condicionais-II-exercicios/exercicio1/script.js
@@ -0,0 +1,28 @@
+// # Exercício 1
+
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+
+const number = Number(
+  prompt("Digite um número para saber se é divisivel por 2 ou 3")
+);
+
+// 1. Utilizando ifs aninhados
+if (number % 2 === 0) {
+  console.log("Ele é divisivel apenas por 2");
+  if (number % 3 === 0) {
+    console.log("Ele é divisivel por 2 e 3");
+  }
+} else {
+  console.log("Ele não é divisivel por nenhum dos dois");
+}
+
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+if (number % 2 === 0 && number % 3 === 0) {
+  console.log("Ele é divisivel por 2 e 3");
+} else if (number % 2 === 0) {
+  console.log("Ele é divisivel apenas por 2");
+} else if (number % 3 === 0) {
+  console.log("Ele é divisivel apenas por 3");
+} else {
+  console.log("Ele não é divisivel por nenhum dos dois");
+}
