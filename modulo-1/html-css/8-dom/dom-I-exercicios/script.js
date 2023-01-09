3b8c36b2622aef11e67e27d190fcf91e2da8e549
Italo Rocha Oliveira <italo.rocha.de.oliveira@gmail.com>
2023-01-09 16:41:02 -0300

    adicionando mais detalhes

--- modulo-1/html-css/8-dom/dom-I-exercicios/script.js
+++ modulo-1/html-css/8-dom/dom-I-exercicios/script.js
@@ -1,16 +1,22 @@
 const frutas = ["laranja", "limão", "uva"];
 
-const fruta1 = document.getElementById("fruta-1");
-const fruta2 = document.getElementById("fruta-2");
-const fruta3 = document.getElementById("fruta-3");
-const fruta4 = document.getElementById("fruta-4");
-const button = document.getElementById("insert");
+function listaDeFrutas() {
+  for (let i in frutas) {
+    let fruta = document.getElementById("fruta-" + (Number(i) + 1));
+    fruta.innerHTML = frutas[i];
+  }
+}
+listaDeFrutas();
 
-fruta1.innerHTML = frutas[0];
-fruta2.innerHTML = frutas[1];
-fruta3.innerHTML = frutas[2];
+const novaLi = () => {
+  let ul = document.getElementById("lista-de-frutas");
+  let newLi = document.createElement("li");
+  newLi.setAttribute("id", "fruta-" + (frutas.length + 1));
+  ul.appendChild(newLi);
+};
 
 const InsertNewFruit = () => {
   frutas.push(document.getElementById("fruit").value);
-  fruta4.innerHTML = frutas[3];
+  listaDeFrutas();
+  novaLi();
 };
