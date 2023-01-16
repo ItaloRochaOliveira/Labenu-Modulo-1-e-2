8c28c30b912d2cb0c357d3d2110d2a93f4c6766f
Italo Rocha Oliveira <italo.rocha.de.oliveira@gmail.com>
2023-01-16 17:37:41 -0300

    modulo 2 componentes material assincrono

---
+++ modulo-2/2-react-componentes/material-assincrono/react-componentes/src/componentes/Main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function Main() {
+  return (
+    <div className="main">
+      <main>
+        <section>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem soluta
+          fugiat inventore blanditiis temporibus optio ut explicabo provident
+          vitae libero eaque eligendi, officiis vero! Consequatur vero repellat
+          animi est dolores.
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          nesciunt illum esse obcaecati, modi odio inventore necessitatibus quae
+          laboriosam mollitia cum voluptates in nisi aliquid nam eaque dolorum
+          exercitationem saepe!
+        </section>
+        <section>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem soluta
+          fugiat inventore blanditiis temporibus optio ut explicabo provident
+          vitae libero eaque eligendi, officiis vero! Consequatur vero repellat
+          animi est dolores.
+        </section>
+      </main>
+    </div>
+  );
+}
