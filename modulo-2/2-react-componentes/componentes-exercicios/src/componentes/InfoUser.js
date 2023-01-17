b4fce1c0d123583acb49952f67e70e0271fe9283
Italo Rocha Oliveira <italo.rocha.de.oliveira@gmail.com>
2023-01-17 17:31:34 -0300

    subindo exercicio de componentes

---
+++ modulo-2/2-react-componentes/componentes-exercicios/src/componentes/InfoUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function InfoUser() {
+  let usuario = "it";
+  return (
+    <div className="info-user">
+      {usuario}
+      <img src="https://picsum.photos/400/400?a=1 " width="30px"></img>
+    </div>
+  );
+}
