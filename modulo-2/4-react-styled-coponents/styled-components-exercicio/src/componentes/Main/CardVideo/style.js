f35f401155c38f978f16858da754845ee320c799
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-01-19 16:01:24 -0300

    subindo exercicio pos feedback

---
+++ modulo-2/4-react-styled-coponents/styled-components-exercicio/src/componentes/Main/CardVideo/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Videos = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    overflow: hidden;
+
+    width: 350px;
+`
+export const ImagemVideo = styled.img`
+    width: 100%;
+    height: 200px;
+    border-radius: 30px;
+`
+
+export const SobreVideo = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    padding: 10px;
+`
