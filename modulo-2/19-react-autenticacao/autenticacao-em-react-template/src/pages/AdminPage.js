61fbe903f7f74067f1a67f7815f57edf6747e29b
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-03-01 11:22:19 -0300

    subindo conceito de autenticação

---
+++ modulo-2/19-react-autenticacao/autenticacao-em-react-template/src/pages/AdminPage.js
@@ -0,0 +1,50 @@
+
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useRequestData from "../hooks/useRequestData";
+import { navigateToLogin } from "../routes/coordinator";
+import { AdminContainer, BoxDetail } from './styleAdminPage'
+
+function AdminPage() {
+  const navigate = useNavigate()
+
+  const { id } = useParams();//use este hook do houter para conseguir fazer a requisição através do id
+  const token = localStorage.getItem("token")
+
+  const headers = {
+    headers: {
+      auth: token
+    }
+  }
+
+  const [dados] = useRequestData({}, `/trip/${id}`, headers) //use este hook para receber as informações da requisição.
+  console.log(dados)
+  const tripDetail = dados.trip
+  console.log(tripDetail)
+
+  useEffect(() => {
+    if(!token){
+      navigateToLogin(navigate)
+    }
+  }, [])
+
+  return (
+    <AdminContainer>
+      <h1>Administração: Detalhes da Viagem</h1>
+      <hr />
+      {
+        tripDetail ?
+          <BoxDetail >
+            <h3>{tripDetail.name}</h3>
+            <p>{tripDetail.description}</p>
+            <p><b>Duração em dias: </b>{tripDetail.durationInDays}</p>
+            <p><b>Data: </b>{tripDetail.date}</p>
+            <p><b>Planeta: </b>{tripDetail.planet}</p>
+          </BoxDetail > :
+          <h2>Escolha uma das viagens disponiveis </h2>
+      }
+    </AdminContainer>
+  );
+}
+
+export default AdminPage;
